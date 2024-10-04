ec2f5b71e4d339ab7785ce49ffd3101327c75899
Arshpreet Singh <arshpreetsingh6@rrc.ca>
2024-10-04 08:44:46 -0500

    Added index view for Products

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/products', to: 'products#index'
+  
   get "products/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
